45b7760a6151d9aa5ee7d3d5117d594845aab00d
Marcelo Vieira <47899094+marcelovieiradesousa@users.noreply.github.com>
2023-09-20 21:12:22 -0300

    Update Remove-The-Word.php

--- Remove-The-Word.php
+++ Remove-The-Word.php
@@ -1,3 +1,4 @@
+#Create a function that takes an array and string. The function should remove the letters in the string from the array, and return the array.
 function removeLetters($letters, $word) {
  	$arrayStr = str_split($word);
  	
