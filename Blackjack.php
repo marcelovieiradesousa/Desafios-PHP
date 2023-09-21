ab534b4559333f3450c66469666d404fa0f1e7e2
Marcelo Vieira <47899094+marcelovieiradesousa@users.noreply.github.com>
2023-09-21 14:34:09 -0300

    Update Blackjack.php

--- Blackjack.php
+++ Blackjack.php
@@ -1,4 +1,4 @@
-##Create a function that takes an array of card numbers and checks if the sum of their value exceeds 21. If the sum exceeds 21, return true and if the sum is under or equal to 21, return false. Values of the cards are as follows:
+<?php ##Create a function that takes an array of card numbers and checks if the sum of their value exceeds 21. If the sum exceeds 21, return true and if the sum is under or equal to 21, return false. Values of the cards are as follows:
 
 #2-10 are their value.
 #J-K (face cards) count as 10.
@@ -26,3 +26,4 @@ function overTwentyOne($cards) {
  	}
 	if($sum > BLACK_JACK) return true; else return  false;
 }
+?>
